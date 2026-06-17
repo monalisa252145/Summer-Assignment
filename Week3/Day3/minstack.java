423c03b9fc598f2303537aa0fc12f37b0db8c169
Monalisa <152963081+monalisa252145@users.noreply.github.com>
2026-06-17 16:51:56 +0530

    Add files via upload

---
+++ Week3/Day3/minstack.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class minstack {
+    
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+
+    }
+    
+    public void push(int value) {
+
+        stack.push(value);
+
+        if(minStack.isEmpty() || value <= minStack.peek()){
+            minStack.push(value);
+        }
+        
+    }
+    
+    public void pop() {
+        int poppedValue = stack.pop();
+        if(poppedValue == minStack.peek())
+        minStack.pop();
+
+        
+    }
+    
+    public int top() {
+
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+        
+    }
+}
